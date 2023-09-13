27071e2e7d3dc1af87253046a790d3fca5f3a44b
Akinyemi Aanuoluwapo Hannah <akinyemihannah26@gmail.com>
2023-09-13 06:14:31 -0700

    commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 let yusuf = "nice"
 console.log(yusuf)
+
+
+
+
+for (let a = 0; a < 5; a++) {
+    if (a === 1)
+        console.log(a)
+    for (let b = 0; b < 5; b++) {
+        console.log(a, b)
+        if (a === 1 && b === 1)
+            break;
+    }
+    break;
+}
